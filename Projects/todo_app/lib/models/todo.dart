f17146db2d430dfbc7e3a77cc00d56666ee3c406
Usman Umar Garba <ugarba202@gmail.com>
2025-08-11 10:19:01 +0100

    todo_app_with_riverpod

---
+++ Projects/todo_app/lib/models/todo.dart
@@ -0,0 +1,34 @@
+// lib/models/todo.dart
+class Todo {
+  final String id;
+  final String title;
+  final bool isCompleted;
+
+  Todo({
+    required this.id,
+    required this.title,
+    this.isCompleted = false,
+  });
+
+  // This is the key method for immutable updates
+  Todo copyWith({
+    String? id,
+    String? title,
+    bool? isCompleted,
+  }) {
+    return Todo(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is Todo && other.id == id;
+  }
+
+  @override
+  int get hashCode => id.hashCode;
+}
